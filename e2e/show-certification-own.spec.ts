8151dc2de83a9da3e45bc7f0c94eb7924a38aa36
Fahdkassim <fahdkassim@gmail.com>
2023-08-18 14:11:33 +0500

    removed the soft assertions and the --headed tag to the playwright workflow file to check if the user gets logged in or not

--- e2e/show-certification-own.spec.ts
+++ e2e/show-certification-own.spec.ts
@@ -4,6 +4,7 @@ let page: Page;
 
 test.beforeAll(async ({ browser }) => {
   page = await browser.newPage();
+  await page.pause();
   await page.goto('http://localhost:8000/');
   await page.getByRole('link', { name: 'Sign in' }).click();
   await page.goto(
@@ -16,25 +17,27 @@ test.afterAll(async () => {
 });
 
 test('while viewing own, should display certificate', async () => {
+  await page.pause();
   await expect(page.locator('text=successfully completed')).toBeVisible();
   await expect(page.locator('text=Responsive Web Design')).toBeVisible();
 });
 
 test('while viewing own, should render a LinkedIn button', async () => {
-  const softExpect = expect.configure({ soft: true });
-  await softExpect(
+  await page.pause();
+  await expect(
     page.locator('text=Add this certification to my LinkedIn profile')
   ).toBeVisible();
 });
 
 test('while viewing own, should render a Twitter button', async () => {
-  const softExpect = expect.configure({ soft: true });
-  await softExpect(
+  await page.pause();
+  await expect(
     page.locator('text=Share this certification on Twitter')
   ).toBeVisible();
 });
 
 test('while viewing own, should be issued with the submission date', async () => {
+  await page.pause();
   await expect(page.locator('[data-cy=issue-date]')).toContainText(
     'Developer Certification on August 3, 2018'
   );
