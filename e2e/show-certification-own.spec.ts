2a8259521965c3d8fe11ebe73aa63bff841b92c0
Fahdkassim <fahdkassim@gmail.com>
2023-08-23 21:08:59 +0500

    refactored the tests for show certificates

--- e2e/show-certification-own.spec.ts
+++ e2e/show-certification-own.spec.ts
@@ -1,37 +1,33 @@
 import { test, expect, type Page } from '@playwright/test';
 
-let page: Page;
-
 test.use({ storageState: 'LoginAuth.json' });
 
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-  await page.goto('/certification/certifieduser/responsive-web-design');
-});
+test.describe('Show certification own', () => {
+  let page: Page;
 
-test.afterAll(async () => {
-  await page.close();
-});
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto('/certification/certifieduser/responsive-web-design');
+  });
 
-test('while viewing own, should display certificate', async () => {
-  expect(await page.isVisible('text=successfully completed')).toBeTruthy();
-  expect(await page.isVisible('text=Responsive Web Design')).toBeTruthy();
-});
+  test.afterAll(async () => {
+    await page.close();
+  });
 
-test('while viewing own, should render a LinkedIn button', async () => {
-  expect(
-    await page.isVisible('text=Add this certification to my LinkedIn profile')
-  ).toBeTruthy();
-});
+  test('should display certificate details', async () => {
+    await expect(page.locator('text=successfully completed')).toBeVisible();
+    await expect(page.locator('text=Responsive Web Design')).toBeVisible();
+    await expect(page.locator('[data-cy=issue-date]')).toContainText(
+      'Developer Certification on August 3, 2018'
+    );
+  });
 
-test('while viewing own, should render a Twitter button', async () => {
-  expect(
-    await page.isVisible('text=Share this certification on Twitter')
-  ).toBeTruthy();
-});
-
-test('while viewing own, should be issued with the submission date', async () => {
-  await expect(page.locator('[data-cy=issue-date]')).toContainText(
-    'Developer Certification on August 3, 2018'
-  );
+  test('should render LinkedIn and Twitter buttons', async () => {
+    await expect(
+      page.locator('text=Add this certification to my LinkedIn profile')
+    ).toBeVisible();
+    await expect(
+      page.locator('text=Share this certification on Twitter')
+    ).toBeVisible();
+  });
 });
